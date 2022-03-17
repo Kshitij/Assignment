48d6f67eda316b7680091e92fe47834b26d393d3
Kshitij <68395829+Kshitij@users.noreply.github.com>
2022-03-17 21:26:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const change = (src) => {
+  document.getElementById("main").src = src;
+};
+
+//Dropdown
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+//Heart
+let button = document.querySelector(".heart-like-button");
+
+button.addEventListener("click", () => {
+  if (button.classList.contains("liked")) {
+    button.classList.remove("liked");
+  } else {
+    button.classList.add("liked");
+  }
+});
+
+//counter
